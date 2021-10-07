d31fcf9ced3c0780794af48d6c47569e0fc81f54
’lcy362‘ <lcy362@163.com>
2021-10-07 16:40:02 +0800

    [ADD]get country of request

---
+++ src/main/java/top/mobility/ip/controller/IpCountryController.java
@@ -0,0 +1,26 @@
+package top.mobility.ip.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.mobility.ip.service.IpCountryService;
+import top.mobility.ip.util.IpUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/address/")
+@RequiredArgsConstructor
+@Slf4j
+public class IpCountryController {
+
+    private final IpCountryService service;
+
+    @GetMapping("/country")
+    public String getCountry(HttpServletRequest request) {
+        String ip = IpUtils.getClientIpAddress(request);
+        return service.getCountryOfIp(ip);
+    }
+}
