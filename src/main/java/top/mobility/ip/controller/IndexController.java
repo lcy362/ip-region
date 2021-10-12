dc5801aeec3c397aeb7786260cef9f149e3abc36
’lcy362‘ <lcy362@163.com>
2021-10-12 18:57:20 +0800

    ADD: pages

---
+++ src/main/java/top/mobility/ip/controller/IndexController.java
@@ -0,0 +1,31 @@
+package top.mobility.ip.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import top.mobility.ip.service.IpCountryService;
+import top.mobility.ip.util.IpUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+public class IndexController {
+
+    private final IpCountryService service;
+
+    @GetMapping("/index")
+    public String showPage(Model model, String ip, HttpServletRequest request) {
+        if (ip != null) {
+            model.addAttribute("result", service.getCountryOfIp(ip));
+        }
+        String ownIp = IpUtils.getClientIpAddress(request);
+        model.addAttribute("ownip", ownIp);
+        model.addAttribute("country", service.getCountryOfIp(ownIp));
+        return "index";
+    }
+
+}
